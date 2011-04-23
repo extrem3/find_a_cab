e02ffe519c4439ba2c95a08e4b61b953e4b55c3d
andr3 <andr3.hack3r@gmail.com>
2011-04-23 22:15:34 +0200

    searching for everything

--- search.php
+++ search.php
@@ -1,10 +1,11 @@
 <?php
 require('config.php');
-$town = $_POST["id"];
-echo '<div class="townName">' . $town . '</div>';
 
 mysql_connect(localhost,$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
+
+$town = $_POST["id"];
+echo '<div class="townName">' . $town . '</div>';
 
 $result = mysql_query("SELECT id_mesto FROM mesta WHERE mesto='$town'");
 $town_id = mysql_result($result, 0);
@@ -23,13 +24,31 @@ while ($row = mysql_fetch_array($result))
 	$userLastName = mysql_result($resultUserName, 0, 1);
 	$resultUserCompanyId = mysql_query("SELECT id_podjetje FROM upor_podj WHERE id_uporabnik='$userId'");
 	$userCompanyId = mysql_result($resultUserCompanyId, 0, 0);
-	$resultUserCompanyName = mysql_query("SELECT naziv,rating,ratings_total FROM podjetje WHERE id_podjetje='$userCompanyId'");
+	$resultUserCompanyName = mysql_query("SELECT naziv,rating,ratings_total,ulica,tel,fax,email,www,opis FROM podjetje WHERE id_podjetje='$userCompanyId'");
 	$userCompanyName = mysql_result($resultUserCompanyName, 0, 0);
 	$userCompanyRating = mysql_result($resultUserCompanyName, 0, 1);
 	$userCompanyRatingTotal = mysql_result($resultUserCompanyName, 0, 2);
+	$userCompanyAddress = mysql_result($resultUserCompanyName, 0, 3);
+	$userCompanyPhone = mysql_result($resultUserCompanyName, 0, 4);
+	$userCompanyFax = mysql_result($resultUserCompanyName, 0, 5);
+	$userCompanyMail = mysql_result($resultUserCompanyName, 0, 6);
+	$userCompanyWebsite = mysql_result($resultUserCompanyName, 0, 7);
+	$userCompanyDesctiption = mysql_result($resultUserCompanyName, 0, 8);
 	if($userCompanyRatingTotal == 0)
 		$userCompanyRatingTotal = 1;
 
-	echo '<div class="driver"><div class="driverName">' . $userName . ' ' . $userLastName . '</div><div class="companyName">' . $userCompanyName . '</div><div class="phoneNumber">' . $phoneNumber . '</div><div class="rating">' . ($userCompanyRating/$userCompanyRatingTotal) . '</div></div>';
+	echo '<div class="driver">';
+	echo '<div class="driverName">' . $userName . ' ' . $userLastName . '</div>';
+	echo '<div class="companyName">' . $userCompanyName . '</div>';
+	echo '<div class="phoneNumber">' . $phoneNumber . '</div>';
+	echo '<div class="rating">' . ($userCompanyRating/$userCompanyRatingTotal) . '</div>';
+	echo '<div class="companyDetails">'; 
+	echo '<div class="companyAddress">' . $userCompanyAddress . '</div>';
+	echo '<div class="companyPhone">' . $userCompanyPhone . '</div>';
+	echo '<div class="companyFax">' . $userCompanyFax . '</div>';
+	echo '<div class="companyMail">' . $userCompanyMail . '</div>';
+	echo '<div class="companyWebsite">' . $userCompanyWebsite . '</div>';
+	echo '<div class="companyDescription">' . $userCompanyDesctiption . '</div>';
+	echo '</div></div>';
 }
 ?>
