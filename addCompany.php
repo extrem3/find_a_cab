4d965148416c203312450c11a37306481d8f0923
andr3 <andr3.hack3r@gmail.com>
2011-04-23 20:21:34 +0200

    Adding users,companys and towns working

--- addCompany.php
+++ addCompany.php
@@ -1,11 +1,74 @@
 <?php
 require('config.php');
 
-$type = "asdf";
-
 mysql_connect($location,$username,$password);
 @mysql_select_db($database) or die( "Unable to select database");
 
+$id_user;
+$id_town;
+$id_company;
+
+
+function addUser()
+{
+	//password checking and hashing to be added still!!!
+	if(mysql_num_rows(mysql_query("SELECT * FROM uporabniki WHERE username= '" . $_POST['username'] . "'"))>0) 
+		return "user with this username already exists";
+	if(mysql_num_rows(mysql_query("SELECT * FROM uporabniki WHERE email= '" . $_POST['email'] . "'"))>0) 
+		return "user with this email already exists";
+	
+	mysql_query("INSERT INTO uporabniki (username, geslo, nivo, ime, priimek, email)
+							   VALUES ('" . $_POST['username'] . "', '" . $_POST['password'] . "', '0', '" . $_POST['name'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "')");
+
+	$result = mysql_query("SELECT max(id_uporabnik) FROM uporabniki");
+	return mysql_result($result, 0, 0);
+}
+function addDriver()
+{
+	//telefonske_st
+	//mesta_telefonske
+	if(mysql_num_rows(mysql_query("SELECT * FROM uporabniki WHERE username= '" . $_POST['username'] . "'"))>0) 
+		return "user with this username already exists";
+	if(mysql_num_rows(mysql_query("SELECT * FROM uporabniki WHERE email= '" . $_POST['email'] . "'"))>0) 
+		return "user with this email already exists";
+	
+	mysql_query("INSERT INTO uporabniki (username, geslo, nivo, ime, priimek, email)
+							   VALUES ('" . $_POST['username'] . "', '" . $_POST['password'] . "', '0', '" . $_POST['name'] . "', '" . $_POST['lastName'] . "', '" . $_POST['email'] . "')");
+
+	$result = mysql_query("SELECT max(ID_uporabnik) FROM uporabniki");
+	return mysql_result($result, 0, 0);
+}
+function addCompany($companyName, $town_id, $user_id)
+{
+	$companyQuery = mysql_query("SELECT id_podjetje FROM podjetje WHERE naziv='$companyName'");
+	if(mysql_num_rows($companyQuery)>0) 
+	{
+		$companyRow = mysql_fetch_assoc($companyQuery);
+		return $companyRow['id_podjetje'];
+	}
+	
+	mysql_query("INSERT INTO podjetje (naziv, ulica, id_mesto, id_drzava, odg_oseba, tel, fax, email, www, opis, rating)
+							   VALUES ('$companyName', '" . $_POST['companyStreet'] . "', '$town_id', '1', '$user_id', '" . $_POST['companyPhone'] . "', '" . $_POST['companyFax'] . "', '" . $_POST['companyMail'] . "', '" . $_POST['companyWebsite'] . "', '" . $_POST['companyDescription'] . "', '0')");
+
+	$result = mysql_query("SELECT max(id_podjetje) FROM podjetje");
+	return mysql_result($result, 0, 0);
+}
+function addTown($town)
+{
+	$town = ucfirst(strtolower($town));
+	$townQuery = mysql_query("SELECT id_mesto FROM mesta WHERE mesto='$town'");
+	if(mysql_num_rows($townQuery)>0) 
+	{
+		$townRow = mysql_fetch_assoc($townQuery);
+		return $townRow['id_mesto'];
+	}
+	
+	mysql_query("INSERT INTO mesta (mesto)
+						    VALUES ('$town')");
+
+	$townResult = mysql_query("SELECT max(id_mesto) FROM mesta");
+	return mysql_result($townResult, 0, 0);
+}
 echo "name: " . $_POST['username'] . "<br>";
 echo "pass: " . $_POST['password'] . "<br>";
 echo "pass2: " . $_POST['passwordCheck'] . "<br>";
@@ -88,73 +151,22 @@ if ($_POST['companyOwner'] == "on")
 echo "<br>";
 echo "<br>";
 echo "<br>";
-
-switch ($type) {
-	case 'company':
-		//add a company and all the data
-
-		$id_user = "23";
-		$company_name = "2taxi1";
-		$street = "2ulica";
-		$person = "2ime pa priimek";
-		$phone = "23131313";
-		$fax = "/";
-		$mail = "r2somemail@somewhere.com";
-		$website = "w2somewhere.com";
-		$description = "lol";
-		$rating = "4";
-		$id_town = '4';
-
-		mysql_query("INSERT INTO podjetje (naziv, ulica, id_mesto, id_drzava, odg_oseba, tel, fax, email, www, opis, rating)
-								   VALUES ('$company_name', '$street', '$id_town', '1', '$person', '$phone', '$fax', '$mail', '$website', '$description', '0')");
-		//dobis id_podjetje, v naslednjem stringu uporabis pri ID_user
-
-		$result = mysql_query("SELECT max(id_podjetje) FROM podjetje");
-		$id_company = mysql_result($result, 0, 0);
-		echo $id_company;
-
-		mysql_query("INSERT INTO telefonske_st (ID_user, telefonske_st)
-								   VALUES ('$id_user', '$phone')");
-		mysql_query("INSERT INTO upor_podj (id_uporabnik, id_podjetje)
-								   VALUES ('$id_user', '$id_company')");
-		//dobis ID_telefonske
-		$result = mysql_query("SELECT max(ID_telefonske_st) FROM telefonske_st");
-		$id_phone = mysql_result($result, 0, 0);
-		//ID_mesta dobis iz 'mesta'
-
-		mysql_query("INSERT INTO mesta_telefonske (ID_mesta, ID_telefonske)
-								   VALUES ('$id_town', '$id_phone')");
-		break;
-	case 'phoneNumber':
-		//add another phone number
-
-		$id_user = "23";
-		$phone = "0123556";
-		$id_town = '4';
-
-		mysql_query("INSERT INTO telefonske_st (ID_user, telefonske_st)
-								   VALUES ('$id_user', '$phone')");
-		$result = mysql_query("SELECT max(ID_telefonske_st) FROM telefonske_st");
-		$id_phone = mysql_result($result, 0, 0);
-		//ID_mesta dobis iz 'mesta'
-
-		mysql_query("INSERT INTO mesta_telefonske (ID_mesta, ID_telefonske)
-								   VALUES ('$id_town', '$id_phone')");
-		break;
-	case 'test':
-		$town = "celJe";
-		if(mysql_num_rows(mysql_query("SELECT * FROM mesta WHERE mesto= '$town'"))>0) 
-		{
-			echo "already in";
-		}else
-		{
-			echo "not in yet";
-		}
-		break;
-	default:
-		echo "type not specified";
-		break;
+$id_user = addUser();
+echo $id_user . "<br>";
+if ($_POST['town'] == "added") 
+{
+	$id_town = addTown($_POST['townSelect']);
+}else
+{
+	$id_town = addTown($_POST['newTown']);
 }
-echo "done";
-
-?>
+echo "town id is: " . $id_town . "<br>";
+if ($_POST['company'] == "added") 
+{
+	$id_company = addCompany($_POST['companySelect'], $id_town, $id_user);
+}else
+{
+	$id_company = addCompany($_POST['companyName'], $id_town, $id_user);
+}
+mysql_query("INSERT INTO upor_podj (id_uporabnik, id_podjetje)
+						    VALUES ('$id_user', '$id_company')");
