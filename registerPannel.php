5fcf76a05de55d7532ddc3a34a4280602da5d444
andr3 <andr3.hack3r@gmail.com>
2011-05-02 22:57:00 +0200

    back and forward buttons working "just as planned"

--- registerPannel.php
+++ registerPannel.php
@@ -3,17 +3,26 @@
 	<title>User pannel</title>
 	<link rel="stylesheet" type="text/css" href="css/slides.css">
 	<script type="text/javascript" src="scr/jquery-1.5.1.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/themes/jquery.ui.all.css">
+	<script type="text/javascript" src="scr/jquery-1.5.1.js"></script>
+	<script type="text/javascript" src="scr/ui/jquery.ui.core.js"></script>
+	<script type="text/javascript" src="scr/ui/jquery.ui.widget.js"></script>
+	<script type="text/javascript" src="scr/ui/jquery.ui.button.js"></script>
+	<script type="text/javascript" src="scr/ui/jquery.ui.position.js"></script>
+	<script type="text/javascript" src="scr/ui/jquery.ui.autocomplete.js"></script>
 	<script type="text/javascript">
 	var total = 0;
 	var slideSpeed = 200;
 	var slideWidth = 730;
 	var currentPosition = 0;
+	var numberOfSlides = 0;
+	var hideButtons = new Array(new Array(1, -1),
+								new Array(3, -1),
+								new Array(5, -1));
 	$(document).ready(function() {
 		var slides = $('.slide');
-		var numberOfSlides = slides.length;
-		$('#navigation').html('<div id="prevSlide">prev</div> <div id="nextSlide">next</div>')
-		$('#prevSlide').css('display', 'inline');
-		$('#nextSlide').css('display', 'inline');
+		numberOfSlides = slides.length;
+		$('#navigation').html('<div class="button" id="prevSlide" ></div><div id="statusBar" /><div class="button" id="nextSlide" >Naprej</div>')
 		$('#content').css('overflow', 'hidden');
 
 		// Wrap all .slides with #slideInner div
@@ -26,31 +35,101 @@
 		// Set #slideInner width equal to total width of all slides
 		$('#slideInner').css('width', slideWidth * numberOfSlides);
 
-		$('#nextSlide').click(
-			function() 
-			{
-				if(currentPosition < numberOfSlides - 1)
-				{
-					++ currentPosition;
-					// Move slideInner using margin-left
-					$('#slideInner').animate({
-						'marginLeft' : slideWidth*(-currentPosition)
-						}, slideSpeed);
-				}
+		$('#nextSlide').click( function() {
+				moveSlideHolder(currentPosition + 1, true);
 			});
-		$('#prevSlide').click(
-			function() 
-			{
-				if(currentPosition > 0)
-				{
-					-- currentPosition;
-					// Move slideInner using margin-left
-					$('#slideInner').animate({
-						'marginLeft' : slideWidth*(-currentPosition)
-						}, slideSpeed);
-				}
+		$('#prevSlide').click( function() {
+				moveSlideHolder(currentPosition - 1, false);
 			});
+
+		$('#content_driver_yes').click( function() {
+				hideButtons[0][1] = 1;
+				moveSlideHolder(currentPosition + 1, true);
+			});
+		$('#content_driver_no').click( function() {
+				hideButtons[0][1] = 0;
+				moveSlideHolder(currentPosition + 2, true);
+			});
+		$('#content_owner_yes').click( function() {
+				hideButtons[1][1] = 1;
+				moveSlideHolder(currentPosition + 1, true);
+			});
+		$('#content_owner_no').click( function() {
+				hideButtons[1][1] = 0;
+				moveSlideHolder(currentPosition + 2, true);
+			});
+
+		updateStatusBar();
 	});
+	function moveSlideHolder(to, forward)
+	{
+		if(to != 0)
+			$('#prevSlide').html('Nazaj');
+		else
+			$('#prevSlide').html('');
+
+		if(to != numberOfSlides - 1)
+			$('#nextSlide').html('Naprej');
+		else
+			$('#nextSlide').html('Zakljuci');
+
+		for(var j = 0; j < hideButtons.length - 1; ++j)
+		{
+			if(hideButtons[j][0] == to && hideButtons[j][1] == -1)
+			{
+				$('#nextSlide').html('');
+			}
+		}
+		if(to >= 0 && to < numberOfSlides)
+		{
+			if(currentPosition == hideButtons[1][0] && !forward && hideButtons[0][1] == 0)
+			{
+				//if we are returning to ALI STE VOZNIK and he said he is not
+				currentPosition = hideButtons[0][0];
+			}else if(to == hideButtons[0][0] + 1 && forward && hideButtons[0][1] == 0)
+			{
+				//if we are going forward from ALI STE VOZNIK and he said he is not
+				currentPosition = hideButtons[1][0];
+			}else if(currentPosition == hideButtons[2][0] && !forward && hideButtons[1][1] == 0)
+			{
+				//if we are returning to DO YOU HAVE A COMPANY and he said he doesn't
+				currentPosition = hideButtons[1][0];
+			}else if(to == hideButtons[1][0] + 1 && forward && hideButtons[1][1] == 0)
+			{
+				//if we are going forward from DO YOU HAVE A COMPANY and he said he doesn't
+				currentPosition = hideButtons[2][0];
+			}else
+			{
+				currentPosition = to
+			}
+			$('#slideInner').animate({
+				'marginLeft' : slideWidth*(-currentPosition)
+				}, slideSpeed);
+			updateStatusBar();
+		}
+	}
+	function updateStatusBar()
+	{
+		$('#statusBar').html("<div class='usedButton'>&nbsp;</div>");
+		$('#statusBar').append("|");
+		var k = 0;
+		for (var i = 0; i < numberOfSlides - 1; ++i)
+		{
+			if (hideButtons[k][0] + 1 == i)
+			{
+				++ k;
+				$('#statusBar').append("|");
+			}
+
+			if (i < currentPosition)
+			{
+				$('#statusBar').append("<div class='usedButton'>&nbsp;</div>");
+			}else
+			{
+				$('#statusBar').append("<div class='unusedButton'>&nbsp;</div>");
+			}
+		}
+	}
 	</script>
 </head>
 <body>
@@ -70,6 +149,8 @@
 		<div id="content_2" class="slide">
 			<h1>Ali ste voznik taksija?</h1><br>
 			<input type="checkbox" name="cabOwner">Taxi driver
+			<div class="button" id="content_driver_yes">Da</div>
+			<div class="button" id="content_driver_no">Ne</div>
 		</div>
 		<div id="content_3" class="slide">
 			<table>
@@ -105,8 +186,10 @@
 			<br>
 		</div>
 		<div id="content_4" class="slide">
-			<h1>Ali ste lastnik podjetja?</h1><br>
+			<h1>Ali je vase podjetje ze v nasi bazi?</h1><br>
 			<input type="checkbox" name="companyOwner">Company owner
+			<div class="button" id="content_owner_yes">Da</div>
+			<div class="button" id="content_owner_no">Ne</div>
 		</div>
 		<div id="content_5" class="slide">
 			<table>
@@ -148,7 +231,7 @@
 			</div>
 		</div>
 	</form>
-	<div id="navigation" />
 	</div>
+	<div id="navigation" />
 </body>
 </html>
