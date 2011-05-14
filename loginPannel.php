fc9edcb350862e6fea9dc1db5ed8602efd9af122
andr3 <andr3.hack3r@gmail.com>
2011-05-14 20:22:48 +0200

    fixed a bug where logout form would bug out

--- loginPannel.php
+++ loginPannel.php
@@ -22,26 +22,35 @@
 		$('#login').append('<div class="button" id="loginButton">Prijava</div>');
 		$('#wrongLogin').html('Ste ze prijavljeni v sistem! <div class="button" id="logoutButton">Odjava</div>');
 		$('#login-button').hide();
-		$('#loginButton').button().click(function() {
-			var formContents = $('#login').serialize();
-			bodyContent = $.ajax({
-				url: "login.php",
-				global: false,
-				type: "POST",
-				data: formContents,
-				dataType: "html",
-				async:false,
-				success: function(msg){
-					$('form#submit').hide();
-					self.parent.window.location = "userPannel.php";
-				}
-			}).responseText;
+		$('input').keypress(function(e){
+			if(e.which == 13)
+			{
+				applyLogin();
+			}
 		});
+
+		$('#loginButton').button().click(applyLogin);
 		$('#logoutButton').button().click(function() {
 			self.parent.window.location = "login.php?logout=true";
 		});
 		$('.button').removeClass('ui-corner-all');
 	});
+	function applyLogin ()
+	{
+		var formContents = $('#login').serialize();
+		bodyContent = $.ajax({
+			url: "login.php",
+			global: false,
+			type: "POST",
+			data: formContents,
+			dataType: "html",
+			async:false,
+			success: function(msg){
+				$('form#submit').hide();
+				self.parent.window.location = "userPannel.php";
+			}
+		}).responseText;
+	}
 	</script>
 </head>
 <body>
@@ -57,7 +66,7 @@
 		echo '<div class="login-pannel">';
 		if(isset($_GET['failed']))
 		{
-			echo '<div id="wrongLogin">Napacno uporabnisko ime ali geslo</div>';
+			echo '<div id="wrongLogin2">Napacno uporabnisko ime ali geslo</div>';
 		}
 		echo '<form id="login" action="login.php" method="POST" accept-charset="utf-8">';
 		echo 'Uporabniško ime:<input type="text" name="username" id="username-input" /><br>';
