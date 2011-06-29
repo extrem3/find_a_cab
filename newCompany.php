c2025eab4013dba02cfaba8f73c3574979d2a6f2
Nejc Males <nejcmales@gmail.com>
2011-06-29 15:41:37 +0200

    improved newCompany

--- newCompany.php
+++ newCompany.php
@@ -17,42 +17,43 @@ require('data.php'); require('checkLogin.php');?>
 		<div id="boxName">Popolna sprememba podjetja<a href="#" id="backLink" onclick="history.go(-1);return false;">Nazaj</a></div>
 		<!-- this code was previously in userPannel.php -->
 		<form id="addCompany" action="user.php?type=addCompany" method="POST" accept-charset="utf-8">
-					Izberite med že vpisanim podjetjem, ali pa vpišite novo:
-					<table id="table">
-						<tr>
-							<td id="inDatabase">
-								<input type="radio" name="company" value="added" checked="true"/>Podjetje je že med ponujenimi
-								<br>
-								<select name="companySelect" id="companySelect">
-									<?php
-									$result = mysql_query("SELECT * FROM podjetje");
-									while($row = mysql_fetch_array($result))
-									{
-										echo "<option value=\"" . $row['naziv'] . "\">" . $row['naziv'] . "</option>";
-									}
-									?>
-								</select>
-							</td>
-							<td id="or">
-								ALI
-							</td>
-							<td id="new">
-								<input type="radio" name="company" value="notAdded"/>Vpišite novo podjetje<br>
-								Naziv podjetja:<input type="text" name="companyName"><br>
-								Ulica:<input type="text" name="companyStreet"><br>
-								Mesto:<input type="text" name="companyTown"><br>
-								Odgovorna oseba:<input type="text" name="companyInCharge"><br>
-								Telefon:<input type="text" name="companyPhone"><br>
-								Fax:<input type="text" name="companyFax"><br>
-								Spletna pošta:<input type="text" name="companyMail"><br>
-								Spletna stran:<input type="text" name="companyWebsite"><br>
-								Opis:<input type="text" name="companyDescription"><br>
-								<input type="submit" value="Spremeni" />
-							</td>
-						</tr>
-					</table>
-					</form>
-		
+			Izberite med že vpisanim podjetjem, ali pa vpišite novo:
+			<table id="table">
+				<tr>
+					<td id="inDatabase">
+						<input type="radio" name="company" value="added" checked="true"/>Podjetje je že med ponujenimi
+						<br>
+						<select name="companySelect" id="companySelect">
+							<?php
+							$result = mysql_query("SELECT * FROM podjetje");
+							while($row = mysql_fetch_array($result))
+							{
+								echo "<option value=\"" . $row['naziv'] . "\">" . $row['naziv'] . "</option>";
+							}
+							?>
+						</select>
+					</td>
+					<td id="or">
+						ALI
+					</td>
+					<td id="new">
+						<input type="radio" name="company" value="notAdded"/>Vpišite novo podjetje<br>
+						<table>
+						<tr><td>Naziv podjetja:</td><td><input type="text" name="companyName"></td></tr>
+						<tr><td>Ulica:</td><td><input type="text" name="companyStreet"></td></tr>
+						<tr><td>Mesto:</td><td><input type="text" name="companyTown"></td></tr>
+						<tr><td>Odgovorna oseba:</td><td><input type="text" name="companyInCharge"></td></tr>
+						<tr><td>Telefon:</td><td><input type="text" name="companyPhone"></td></tr>
+						<tr><td>Fax:</td><td><input type="text" name="companyFax"></td></tr>
+						<tr><td>Spletna pošta:</td><td><input type="text" name="companyMail"></td></tr>
+						<tr><td>Spletna stran:</td><td><input type="text" name="companyWebsite"></td></tr>
+						<tr><td>Opis:</td><td><input type="text" name="companyDescription"></td></tr>
+						<tr><td>&nbsp;</td><td><input type="submit" value="Spremeni" /></td></tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</form>
 	</div>
 	<div id="bottom">&nbsp;</div> <!-- leave this alone -->
 	<div id="footer">
